4bf6b89b17dd0b87e385f3e0c727acdd9e4357e5
masafumi24 <konchan24karats@gmail.com>
2020-11-30 18:35:09 +0900

    その他のアイテムも保存できるようにした

--- app/controllers/others_controller.rb
+++ app/controllers/others_controller.rb
@@ -1,8 +1,25 @@
 class OthersController < ApplicationController
   def new
-    @other = Other.new
+    @other = OthersFormObject.new
   end
 
   def create
+    @other = OthersFormObject.new(others_params)
+    @other.save ? (redirect_to root_path) : (render "new")
+  end
+
+  private
+
+  def others_params
+    params.require(:other).permit(
+      :image,
+      :purchase_date,
+      :prefecture_id,
+      :shop_id,
+      :brand_name,
+      color_ids: [],
+      season_ids: [],
+      brand_ids: []
+    )
   end
 end
