19d23fce5fd511c523bd203f25b52808232675b1
masafumi24 <konchan24karats@gmail.com>
2020-12-05 23:20:37 +0900

    closet#indexに要素を追加

---
+++ app/assets/javascripts/closet_items_slide_image_opacity_plate.js
@@ -0,0 +1,6 @@
+$(function() {
+  const target_block = $('#back_image')
+  const target_block_height = $('#back_image').height()
+  const target_block_width = $('#back_image').width()
+  $('#opacity_plate').height(target_block_height).width(target_block_width);
+});
