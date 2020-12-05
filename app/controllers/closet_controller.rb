bb845247ef75de4849e2e2059efe444269c5e996
masafumi24 <konchan24karats@gmail.com>
2020-12-05 23:20:02 +0900

    六つの種別の中からそれぞれ最後に保存されたアイテムを取得する

--- app/controllers/closet_controller.rb
+++ app/controllers/closet_controller.rb
@@ -1,5 +1,12 @@
 class ClosetController < ApplicationController
   def index
+    @new_6_data = []
+    @new_6_data << Top.last(1)
+    @new_6_data << Outerwear.last(1)
+    @new_6_data << Pant.last(1)
+    @new_6_data << Shoe.last(1)
+    @new_6_data << Accessory.last(1)
+    @new_6_data << Other.last(1)
     # グラフ用の色データ、背景色を取得
     @color_graph_data = Color.color_data_acquisition_for_graphs
     @color_background_colors = Color.background_color_data_acquisition_for_graphs(@color_graph_data)
